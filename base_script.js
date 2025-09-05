32c18642fa4db45b8712c9934604bf49a17c616b
Md Eftequarul Alam <efthaqur@gmail.com>
2025-09-05 16:19:10 +0600

    warning and ternary condition added

--- base_script.js
+++ base_script.js
@@ -16,15 +16,25 @@ const displayLevelWord = (allWords) => {
     const wordContainer = document.getElementById("word-container");
      wordContainer.innerHTML ="";
 
+     if(allWords.length == 0){
+        wordContainer.innerHTML = `
+        <div class="text-center col-span-full rounded-xl py-10 space-y-6">
+            <img class="mx-auto" src="./assets/alert-error.png" alt="alert_error">    
+            <p class="tiro-bangla-regular-italic text-xl text-gray-500 font-medium">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="noto-serif-bengali-font text-4xl font-bold ">পরবর্তী বা পূর্ববর্তী Lesson এ যান</h2>
+        </div>`;
+        return;
+     }
+
     allWords.forEach( getWord => {
         console.log(getWord);
         const card = document.createElement("div");
         card.innerHTML = `
             <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="poppins-bold text-xl">${getWord.word}</h2>
-            <p class="inter-fonts fonts-semibold">Meaning / Pronounciation</p>
-            <div class="tiro-bangla-regular text-2xl font-medium">"${getWord.meaning} / ${getWord.pronunciation}"</div>
-                <div class="flex justify-between itemes-center">
+            <h2 class="poppins-bold text-xl">${getWord.word ? getWord.word : "Word Not Found!"}</h2>
+            <p class="inter-fonts fonts-bold">Meaning / Pronounciation</p>
+            <div class="tiro-bangla-regular text-2xl font-medium">"${getWord.meaning ? getWord.meaning : "অর্থ পাওয়া যায়নি"} / ${getWord.pronunciation ? getWord.pronunciation : "উচ্চারণ পাওয়া যায়নি"}"</div>
+                <div class="flex justify-between items-center">
                     <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
                         <i class="fa-solid fa-circle-info"></i>
                     </button>
